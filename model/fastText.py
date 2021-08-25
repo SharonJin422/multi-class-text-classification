35c57960fb7e3d6a45806ca92a2f8b471975590b
SharonJin422 <85158796+SharonJin422@users.noreply.github.com>
2021-08-25 20:37:23 +0800

    Update fastText.py

--- model/fastText.py
+++ model/fastText.py
@@ -1,45 +1,20 @@
+import pandas as pd
 import numpy as np
-
-from tqdm import tqdm
-from fastText import train_supervised
-from sklearn.metrics import classification_report
-
-
-'''
-参考链接
-    https://github.com/facebookresearch/fastText
-    https://github.com/facebookresearch/fastText/blob/master/python/doc/examples/train_supervised.py
-'''
-
-
-def print_results(N, p, r):
-    print("N\t" + str(N))
-    print("P@{}\t{:.3f}".format(1, p))
-    print("R@{}\t{:.3f}".format(1, r))
+import joblib
+from sklearn.metrics import f1_score
+import fasttext
+import pdb
+# 转换为FastText需要的格式
+train_df = joblib.load(open('data/train_data.p','rb'))
+train_df['label'] = [np.argmax(i) for i in train_df['label']]
+train_df.to_csv('train_data_set.csv', index=None,header=None, sep='\t')
+# pdb.set_trace()
+train_df['label_ft'] = '__label__' + train_df['label'].astype(str)
+train_df[['text','label_ft']].iloc[:-10000].to_csv('train.csv', index=None, header=None, sep='\t')
 
 
-if __name__ == "__main__":
-    train_data = '../data/fastTextData/train_data'
-    valid_data = '../data/fastTextData/valid_data'
+model = fasttext.train_supervised('train.csv', lr=0.55, lrUpdateRate =100, wordNgrams=1,ws=5, dim=100,bucket=10000000,
+                                  verbose=2, minCount=1, epoch=100, loss="hs")
 
-    test_data = [line.strip().split('\t')[1] for line in open(valid_data, "r")]
-    test_label = [line.strip().split('\t')[0] for line in open(valid_data, "r")]
-    model = train_supervised(input=train_data,
-                             dim=100,
-                             lr=0.1,
-                             wordNgrams=2,
-                             minCount=1,
-                             bucket=10000000,
-                             epoch=6,
-                             thread=4,
-                             label='__label__')
-
-    print(np.array(test_data).shape)
-    print(np.array(test_label).shape)
-    print(model.test("../data/fastTextData/valid_data"))
-
-    predict_label = []
-    for line in tqdm(test_data):
-        result, proba = model.predict(line)
-        predict_label.append(result[0])
-    print(classification_report(y_pred=predict_label, y_true=test_label))
+val_pred = [model.predict(x)[0][0].split('__')[-1] for x in train_df.iloc[-10000:]['text']]
+print(f1_score(train_df['label'].values[-10000:].astype(str), val_pred, average='macro'))   # result: 19.2%
