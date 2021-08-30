140e87ae3167314a2e57fa94040c3442de7c406b
SharonJin422 <85158796+SharonJin422@users.noreply.github.com>
2021-08-30 09:46:09 +0800

    Update generate_word_embeddings.py

--- generate_word_embeddings.py
+++ generate_word_embeddings.py
@@ -2,7 +2,7 @@ from gensim.models.word2vec import KeyedVectors
 from tensorflow.keras.preprocessing.text import text_to_word_sequence,one_hot,Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import jieba_fast as jieba
-import joblib
+import joblib, json
 import collections,pdb
 from gensim.models.word2vec import KeyedVectors
 from gensim import corpora, similarities, models, matutils, utils
@@ -44,35 +44,9 @@ def RemoveWordAppearBelowN(corpora_documents, n = 1):
     corpora_documents = [[token for token in text if frequency[token] > n] for text in corpora_documents]
     return corpora_documents
 
-if __name__ == '__main__':
-    wv_from_text = KeyedVectors.load_word2vec_format('./data/Tencent_AILab_ChineseEmbedding.txt', binary=False)
-    train = joblib.load(open('data/train_data.p','rb'))
-    test = joblib.load(open('data/test_data.p','rb'))
-    x_text = train['text']
-    y_text = test['text']
-    all_text = x_text.append(y_text)
-    print(type(x_text))
-    print(len(train), len(test), len(all_text))
-    token_list = jieba_tokenize(all_text)
-
+def generate_word_embeddings(dictionary):
     wv_from_text = KeyedVectors.load_word2vec_format('./data/Tencent_AILab_ChineseEmbedding.txt', binary=False)
     joblib.dump(wv_from_text, './dictionary/embeddingMatrix.dict')
-    dictionary = corpora.Dictionary(token_list) #{index:vocab}
-    print("before len", len(dictionary))
-    dictionary.filter_extremes(no_below = 5, no_above=1.0, keep_n =None ) #n=2 111607
-
-    print("after len", len(dictionary))
-    dictionary.filter_n_most_frequent(5)
-    dictionary.compactify()
-    print("after filter_n_most_frequent len", len(dictionary))
-    token2id = dictionary.token2id # {vocab:idx}
-    joblib.dump(dictionary, './dictionary/idx2vocaburary_test_train.dict')
-    joblib.dump(token2id, './dictionary/token2id_test_train.dict')
-    print("vocab size", len(dictionary)) #before filter:227728, after filter:[n=3]78799, [n=5]54736
-    print(dictionary.keys()[:10])
-    print(dictionary.get(3))
-    print(list(token2id.keys())[:10])
-    print(token2id['中国'])
     print(wv_from_text['中国'])
     embeddingDim = 200
     embeddingList = np.random.uniform(-1, 1,[(len(dictionary) + 2), embeddingDim])
@@ -83,3 +57,32 @@ if __name__ == '__main__':
         except:
             print("token is not found in embedding matrix", token, "frq",dictionary.dfs[token2id[token]])
     joblib.dump(embeddingList, './data/embedding_weights.list')
+
+if __name__ == '__main__':
+    train = joblib.load(open('data/train_data.p','rb'))
+    test = joblib.load(open('data/test_data.p','rb'))
+    x_text = train['text']
+    y_text = test['text']
+    all_text = x_text.append(y_text)
+    token_list = jieba_tokenize(all_text)
+    dictionary = corpora.Dictionary(token_list) #{index:vocab}
+    print("before len", len(dictionary))
+    dictionary.filter_extremes(no_below = 5, no_above=1.0, keep_n =None ) #n=2 111607
+    print("after len", len(dictionary))
+
+    dictionary.filter_n_most_frequent(5)
+    dictionary.compactify()
+    print("after filter_n_most_frequent len", len(dictionary))
+
+    token2id = dictionary.token2id # {vocab:idx}
+    joblib.dump(dictionary, './dictionary/idx2vocaburary_test_train.dict')
+    joblib.dump(token2id, './dictionary/token2id_test_train.dict')
+    print("vocab size", len(dictionary)) #before filter:227728, after filter:[n=3]78799, [n=5]54736
+    print(dictionary.keys()[:10])
+    print(dictionary.get(3))
+    print(list(token2id.keys())[:10])
+    print(token2id['中国'])
+
+    params = json.loads(open('parameters.json').read())
+    if params['use_embeddings']:
+        generate_word_embeddings(dictionary)
